37a75175929c84a51b4db127b43e0ebbd74f1362
leodobrovolsky <leodobrovolsky.com@gmail.com>
2019-12-08 23:10:27 +0200

    lines

--- src/mx_lines_to_structs.c
+++ src/mx_lines_to_structs.c
@@ -1,5 +1,18 @@
 #include "pathfinder.h"
 
+static void path_clean(t_path **path, int path_number) {
+    for (int i = 0; i < path_number; i++) {
+        for (int j = 0; j < path_number; j++) {
+            if (mx_path_cmp(path[i], path[j], 1) 
+                && mx_path_cmp(path[i], path[j], 2)
+                && path[i] -> dist > path[j] -> dist) {
+                    path[i] -> dist = path[j] -> dist;               
+            }
+        }
+    }
+}
+
+
 t_path **mx_lines_to_structs(char **lines, int lines_number) {
     t_path **path = (t_path**)malloc(sizeof(t_path*) * (lines_number * 2 + 1));
     char *island1 = NULL;
@@ -20,5 +33,6 @@ t_path **mx_lines_to_structs(char **lines, int lines_number) {
         mx_del_line(&island1, &island2, &dist);
     }
     path[lines_number * 2] = NULL;
+    path_clean(path, lines_number * 2);
     return path;
 }
